b635e0592f8c97ccabd3ace2a2b01c72635eacac
Venkateshdosapatni <147462567+Venkateshdosapatni@users.noreply.github.com>
2024-09-15 13:05:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,167 @@
+let Input = document.getElementById("text")
+
+function myred(){
+    Input.style.color = "red"
+} 
+function mygrey(){
+    Input.style.color = "grey"
+}
+
+function mysilver(){
+    Input.style.color = "silver"
+}
+
+function mymaroon(){
+    Input.style.color = "maroon"
+}
+
+function mypurple(){
+    Input.style.color = "purple"
+}
+
+function mygreen(){
+    Input.style.color = "green"
+}
+
+function mylime(){
+    Input.style.color = "lime"
+}
+
+function myolive(){
+    Input.style.color = "olive"
+}
+
+function myyellow(){
+    Input.style.color = "yellow"
+}
+
+function myyellowgreen(){
+    Input.style.color = "yellowgreen"
+}
+
+function mynavy(){
+    Input.style.color = "navy"
+}
+
+function mypurple(){
+    Input.style.color = "purple"
+}
+
+function myteal(){
+    Input.style.color = "teal"
+}
+
+function myaqua(){
+    Input.style.color = "aqua"
+}
+
+function mybeige(){
+    Input.style.color = "beige"
+}
+
+function mybisque(){
+    Input.style.color = "bisque"
+}
+
+function myblueviolet(){
+    Input.style.color = "blueviolet"
+}
+
+function mybrown(){
+    Input.style.color = "brown"
+}
+
+function mycadetblue(){
+    Input.style.color = "cadetblue"
+}
+
+function mychocolate(){
+    Input.style.color = "chocolate"
+}
+
+
+
+
+
+function myred1(){
+    Input.style.backgroundColor = "red"
+} 
+function mygrey1(){
+    Input.style.backgroundColor = "grey"
+}
+
+function mysilver1(){
+    Input.style.backgroundColor = "silver"
+}
+
+function mymaroon1(){
+    Input.style.backgroundColor = "maroon"
+}
+
+function mypurple1(){
+    Input.style.backgroundColor = "purple"
+}
+
+function mygreen1(){
+    Input.style.backgroundColor = "green"
+}
+
+function mylime1(){
+    Input.style.backgroundColor = "lime"
+}
+
+function myolive1(){
+    Input.style.backgroundColor = "olive"
+}
+
+function myyellow1(){
+    Input.style.backgroundColor = "yellow"
+}
+
+function myyellowgreen1(){
+    Input.style.backgroundColor = "yellowgreen"
+}
+
+function mynavy1(){
+    Input.style.backgroundColor = "navy"
+}
+
+function mypurple1(){
+    Input.style.backgroundColor = "purple"
+}
+
+function myteal1(){
+    Input.style.backgroundColor = "teal"
+}
+
+function myaqua1(){
+    Input.style.backgroundColor = "aqua"
+}
+
+function mybeige1(){
+    Input.style.backgroundColor = "beige"
+}
+
+function mybisque1(){
+    Input.style.backgroundColor = "bisque"
+}
+
+function myblueviolet1(){
+    Input.style.backgroundColor = "blueviolet"
+}
+
+function mybrown1(){
+    Input.style.backgroundColor = "brown"
+}
+
+function mycadetblue1(){
+    Input.style.backgroundColor = "cadetblue"
+}
+
+function mychocolate1(){
+    Input.style.backgroundColor = "chocolate"
+}
+function reset(){
+    Input.style.color = "black"
+    Input.style.backgroundColor ="rgb(246, 239, 229)"
+}
